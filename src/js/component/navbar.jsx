183231c81760273877edc8010ce758679b20d1d4
Pablo <143550869+arkngel212@users.noreply.github.com>
2024-04-15 18:15:41 +0000

    Create [Navbar]

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+//create your first component
+const Navbar = () => {
+	return (
+		<nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-end" id="navbarsExample02">
+        <ul class="navbar-nav d-flex justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+	);
+};
+
+export default Navbar;
+
