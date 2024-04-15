8db90869ef593873df759fe22b0141d68ace95e3
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-04-15 19:30:42 +0000

    footer y home

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+//create your first component
+const Footer = (props) => {
+    return (
+        <div className="bg-dark py-4 text-light text-center">
+           <p className="mb-0">Copyright @ 4Geeks 2024</p>
+        </div>
+    );
+};
+
+
+export default Footer
